abddc4350508471375884eb6881e89a8bbb1910f
longy99 <139186941+longy99@users.noreply.github.com>
2023-09-07 09:39:16 -0400

    Updated ami ID variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0940df33750ae6e7f" 
+    default     = "ami-0ee23bfc74a881de5" 
 }
 variable "versioning" {
     type        = bool
